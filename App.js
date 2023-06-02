884dfd2a27bbb5626314d15166fe8e4c3f03b43c
WiamMerjane <116950948+WiamMerjane@users.noreply.github.com>
2023-06-02 11:42:09 +0100

    Update App.js

--- App.js
+++ App.js
@@ -3,20 +3,71 @@ import { StyleSheet, View, ScrollView } from 'react-native';
 import Header from './Header';
 import Footer from './Footer';
 import Body from './Body';
+import ContactPage from './ContactPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <View style={styles.container}>
       <Header />
-      <Body />
-      <Footer />
-    </ScrollView>
+      <NavigationContainer>
+        <View style={styles.contentContainer}>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'Pharmacies') {
+                  iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+                } else if (route.name === 'Contacts') {
+                  iconName = 'ios-list';
+                }
+
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: 'green',
+              tabBarInactiveTintColor: 'gray',
+            })}
+          >
+            <Tab.Screen name="Pharmacies">
+              {() => (
+                <ScrollView contentContainerStyle={styles.scrollContainer}>
+                  <Body />
+                </ScrollView>
+              )}
+            </Tab.Screen>
+            <Tab.Screen name="Contacts">
+              {() => (
+                <ScrollView contentContainerStyle={styles.scrollContainer}>
+                  <ContactPage />
+                </ScrollView>
+              )}
+            </Tab.Screen>
+          </Tab.Navigator>
+        </View>
+      </NavigationContainer>
+      
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    
+  },
+  contentContainer: {
+    flex: 7,
+    
+  },
+  scrollContainer: {
     flexGrow: 1,
-    paddingBottom: 20,
+    paddingHorizontal: 16,
   },
 });
